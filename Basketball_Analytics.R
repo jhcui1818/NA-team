ce18652433023568161962733fa689545e84fdc6
jhcui1818 <jhcui1818@users.noreply.github.com>
2018-07-17 08:40:38 -0400

    team

--- Basketball_Analytics.R
+++ Basketball_Analytics.R
@@ -37,29 +37,40 @@ order_play <- play_by_play %>%
 # merge datasets
 gm_id <- unique(game_lineup$Game_id)
 merged_play <- list()
-
-
 for (i in 1:50) {
   game_player <- unique(filter(game_lineup, game_lineup$Game_id == gm_id[i])[,3:4])
   game_event <- df_scored %>% 
     filter(Game_id == gm_id[i])
   merged_play[[i]] <- left_join(game_event, game_player, by = c("Person1"="Person_id"))
 }
+for (i in 1:50) {
+  game_player <- unique(filter(game_lineup, game_lineup$Game_id == gm_id[i])[,3:4])
+  game_event <- df_scored %>% 
+    filter(Game_id == gm_id[i])
+  merged_play[[i]] <- left_join(merged_play[[i]], game_player, by = c("Person2"="Person_id"))
+}
 library(data.table)
 df_merged_play <- rbindlist(merged_play)
-
+colnames(df_merged_play)[7] <- "Original_Team_id"
+colnames(df_merged_play)[10] <- "Person1_Team_id"
+colnames(df_merged_play)[11] <- "Person2_Team_id"
 game_team <- game_lineup %>%
   group_by(Game_id, Team_id) %>%
   summarise(n=n())
 game_team$Team <- rep(c("team1", "team2"),50)
 library(tidyr)
-
 game_team <- game_team %>% spread(Team, Team_id)
 df_merged_play <- left_join(df_merged_play, game_team[,c(1,3,4)], by = "Game_id")
+df_merged_play[which(is.na(df_merged_play$Person1_Team_id) & df_merged_play$Event_Msg_Type == 8),"Person1_Team_id"] <- df_merged_play[which(is.na(df_merged_play$Person1_Team_id) & df_merged_play$Event_Msg_Type == 8),"Person2_Team_id"]  
+df_merged_play[which(is.na(df_merged_play$Person1_Team_id) & df_merged_play$Event_Msg_Type == 1),"Person1_Team_id"] <- df_merged_play[which(is.na(df_merged_play$Person1_Team_id) & df_merged_play$Event_Msg_Type == 1),"Person2_Team_id"]  
+
+##################################
 sub_id <- df_merged_play %>%
-  filter(Event_Msg_Type == 8 & is.na(Team_id.y) == TRUE)
+  filter(Event_Msg_Type == 8 & is.na(Person1_Team_id) == TRUE)
+make_id <- df_merged_play %>%
+  filter(Event_Msg_Type == 1 & is.na(Person1_Team_id) == TRUE)
 na_team_id <- df_merged_play %>%
-  filter(is.na(Team_id.y) == TRUE)
+  filter(is.na(Person1_Team_id) == TRUE)
 a <- unique(game_lineup[,3:4])
 b <- unique(game_lineup[,c(1,3,4)])
 c <- left_join(a, b, by = c("Person_id", "Team_id"))
@@ -68,3 +79,12 @@ rep_a <- a %>%
   summarise(n=n()) %>%
   filter(n == 2) %>%
   left_join(b, by = "Person_id")
+
+sub_id_line <- df_merged_play[which(is.na(df_merged_play$Team_id.y) & df_merged_play$Event_Msg_Type == 8), "Team_id.y"]
+colnames(df_merged_play)[7] <- "Original_Team_id"
+colnames(df_merged_play)[10] <- "Person1_Team_id"
+df_merged_play2 <- left_join(df_merged_play, game_team)
+
+t <- df_merged_play[-which(is.na(df_merged_play$Person2_Team_id)),]
+identical(t$Person1_Team_id, t$Person2_Team_id)
+#person1 and person2 are always in the same team
